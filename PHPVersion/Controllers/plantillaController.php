0fddfe13a1f8b662e2de10d1d9372333b471ba25
sdelsalto <sdelsalto@emagic.fin.ec>
2022-01-27 15:20:03 -0500

    Added AdminLTE template to the project

---
+++ PHPVersion/Controllers/plantillaController.php
@@ -0,0 +1,6 @@
+<?php
+    class Plantilla{
+        public function LlamarPlantilla(){
+            include "Views/plantilla.php";
+        }
+    }
---
+++ PHPVersion/index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "Controllers/plantillaController.php";
+    $plantilla = new Plantilla();
+    $plantilla -> LlamarPlantilla();
+?>
