277a4c92f8fca615239d763f2509d34525f3fca1
srdelsalto <srdel@espe.edu.ec>
2022-01-27 20:07:53 -0500

    Created session and Login Page

---
+++ PHPVersion/Views/modules/login.php
@@ -0,0 +1,29 @@
+<div class="login-box">
+  <div class="login-logo">
+    <img src="Views/img/logo 2.png" class="img-responsive">
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Inicio de Sesión</p>
+
+    <form method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email" name="libreta">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password" name="password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
